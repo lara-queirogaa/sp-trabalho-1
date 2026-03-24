7c388b9acff13cd26de4ea5327c74a9ced93e3a7
Lara Queiroga <lara.queiroga06@gmail.com>
2026-03-24 22:31:37 +0000

    tirei prints

--- main.py
+++ main.py
@@ -2,18 +2,14 @@ from aes import run_aes, plot_aes
 from rsa2048 import run_rsa, plot_rsa
 from sha256 import run_sha, plot_sha
 
-print("Running AES...")
 run_aes()
 plot_aes()
 
-print("Running RSA...")
 run_rsa()
 plot_rsa()
 
-print("Running SHA-256...")
 run_sha()
 plot_sha()
 
-print("Running comparisons...")
 import aes_vs_rsa
 import aes_vs_sha256
